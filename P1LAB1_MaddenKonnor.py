a3043ed78d29a5d85d6d52e83337a79aba34b217
KonnorMadden <132110899+KonnorMadden@users.noreply.github.com>
2023-04-28 20:47:43 -0400

    First submission

---
+++ P1LAB1_MaddenKonnor.py
@@ -0,0 +1,8 @@
+# taking the first name input from the user
+first_name = input()
+
+# printing the welcome message to the user using f-string to print the name in the message that is inputted by the user and not printing a hard-coded name
+# f-string helps to add variables inside the string itself using curly braces {}
+# we will insert the variable first_name in our f-string
+
+print(f'Hello {first_name} and welcome to CS Online!')
